c122f5cabaed4cdefd0576c7ad036d107462d375
zhe.cao <zhe.cao@mail.rakuten.com>
2013-08-22 09:50:35 +0900

    code formated

--- my_app.rb
+++ my_app.rb
@@ -6,40 +6,40 @@ class ::Logger; alias_method :write, :<<; end
 
 class MyApp < Sinatra::Application
 
-	configure :production do
-		# disable sinatra logging. use self defiend logger
-		disable :logging
+    configure :production do
+        # disable sinatra logging. use self defiend logger
+        disable :logging
 
-		$log = ::Logger.new("log/app.log", "daily")
-		$log.level = Logger::INFO
-		use Rack::CommonLogger, $log
-	end
-	
-	configure :testing do
-		# disable sinatra logging. use self defiend logger
-		disable :logging
+        $log = ::Logger.new("log/app.log", "daily")
+        $log.level = Logger::INFO
+        use Rack::CommonLogger, $log
+    end
 
-		$log = ::Logger.new(STDOUT)
-		$log.level = Logger::INFO
-		use Rack::CommonLogger, $log
-	end
-	
-	configure :development do
-		# disable sinatra logging. use self defiend logger
-		disable :logging
+    configure :testing do
+        # disable sinatra logging. use self defiend logger
+        disable :logging
 
-		$log = ::Logger.new(STDOUT)
-		$log.level = Logger::DEBUG
-		use Rack::CommonLogger, $log
-	end
+        $log = ::Logger.new(STDOUT)
+        $log.level = Logger::INFO
+        use Rack::CommonLogger, $log
+    end
 
-	get('/') do 
-		$log.debug {"debg log"} 
-		$log.info {"info log"}
-		$log.warn {"info log"}
-		"Hello, sinatra!"
-	end
+    configure :development do
+        # disable sinatra logging. use self defiend logger
+        disable :logging
 
-	# start the server if ruby file executed directly
-	run! if __FILE__ == $0
+        $log = ::Logger.new(STDOUT)
+        $log.level = Logger::DEBUG
+        use Rack::CommonLogger, $log
+    end
+
+    get('/') do 
+        $log.debug {"debg log"} 
+        $log.info {"info log"}
+        $log.warn {"info log"}
+        "Hello, sinatra!"
+    end
+
+    # start the server if ruby file executed directly
+    run! if __FILE__ == $0
 end
